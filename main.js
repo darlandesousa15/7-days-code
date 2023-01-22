eaa01f3143263daa4e66afd9ef6416785677f916
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-22 06:02:59 -0300

    perfecting the prompt

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let stringDez = '10'
 const nome = prompt("Qual o seu nome ?");
 const idade = prompt("Quantos anos você tem?");
 const programacao = prompt("Qua linguagem de programação você esta aprendendo ?");
+const estudando = prompt("Você gosta de estudar " + programacao + "? Responda com o número 1 para sim ou 2 para não.");
 /*
 const cidade = prompt("Digite a sua cidade:");
 const msg = `Você é de ${cidade}!`;
@@ -17,6 +18,15 @@ function escreveTela(){
   document.write("Olá " + nome + ",você tem " + idade + " anos e já está aprendendo " + programacao);
 }
 escreveTela();
+
+function linguagem(){
+  if(estudando == "1"){
+alert("Muito bom! Continue estudando e você terá muito sucesso.");
+  } else {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+  }
+}
+linguagem();
 
 if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
