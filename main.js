412a01106586656c8962e1d3c25a5bb57fcecbe3
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-24 05:39:51 -0300

    return html for begin and add lines of comander to js

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-const controle = document.querySelectorAll("[data-front]");
-
-controle.forEach( (elemento) => {elemento.addEventListener("click", (evento) => 
-proximaMenssagem (evento.target.dataset.controle, evento.target.parentNode))
-})
 
 
-function proximaMenssagem (igual, controle){
-  
-  if ( igual === "front" ) {
-    alert("deu certo")
-    
-  } else {
-    alert("deu errado")
-  }
+
+function alertando() {
+  alert("Selecione 1 Para a primeira opção e 2 para a segunda opção");
+  document.write("<h1>Selecione 1 Para a primeira opção e 2 para a segunda opção</h1>");
 }
+
+alertando();
+
+const perguntaFront = prompt("Você quer seguir para área de Front-End ou seguir para a área de Back-End ?");
+
+function frontBack () {
+  perguntaFront;
+}
+
+frontBack();
+
+function frontIfElse (){
+  if (perguntaFront === "1" || perguntaFront == "front" ){
+    alert("deu certo");
+  } else {
+    alert("VALOR INFORMADO INCORRETO - INFORME 1 PARA A PRIMEIRA OPÇÃO E 2 PARA A SEGUNDA");
+    document.write("<b><h1>***RECOMESSE***<br>VALOR INFORMADO INCORRETO - INFORME 1 PARA A PRIMEIRA OPÇÃO E 2 PARA A SEGUNDA</h1></b>")
+  }
+  }
+frontIfElse();
