34e6f4bf1d10931f3d48efe0dba90961b3e1f502
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-25 05:53:01 -0300

    Resolving loops

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
 
-
 function alertando() {
   alert("Selecione 1 Para a primeira opção e 2 para a segunda opção ou escreva sua resposta sem erros ortográficos");
   document.write("<h1>Selecione 1 Para a primeira opção e 2 para a segunda opção.<br>Ou escreva sua resposta sem erros ortográficos</h1>");
 }
-
 alertando();
 
 const perguntaFront = prompt("Você quer seguir para área de Front-End ou seguir para a área de Back-End ?");
@@ -26,14 +24,23 @@ function frontIfElse (){
   }
 frontIfElse();
 
-const contador = 0;
-
 function determinandoEscolhas (){
-  if (teste === "1" || teste == "react" || teste == "vue" || teste === "2"|| teste == "back-end" || teste == "c" || teste == "c#" ){
-    prompt('tbm deu certo')
+  if (teste == "react" || teste == "vue" || teste === "1" || teste === "2" || teste === "3" || teste === "4" || teste == "back-end" || teste == "java" || teste == "c" || teste == "c#" ){
+    prompt('Quais são as tecnologias nas quais você gostaria de se especializar ou de conhecer?')
   } else {
     alert("VALOR INFORMADO INCORRETO - INFORME 1 PARA A PRIMEIRA OPÇÃO E 2 PARA A SEGUNDA");
+    contador = 2;
   }
 }
 determinandoEscolhas();
+
+for(let contador = 0; contador <= 8; contador ++){
+  const questionandoTecnologias = prompt("Possui outras tecnologias de interesse ? Caso não possua digiter 'sair'");
+  if (questionandoTecnologias == "sair" || contador == "10"){
+    alert("Parabéns, todas respondidas com sucesso!!!");
+    contador == "8";
+    break;
+  } 
+}
+document.write("<br>SEUS DADOS FORAM GRAVADOS, OBRIGADO POR PARTICIPAR<br><h2>***PARABÉNS***</h2>");
 
