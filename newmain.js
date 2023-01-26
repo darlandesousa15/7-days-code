2e5e8284886c109909ccf4babb9225fe6f135bc3
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-26 05:00:29 -0300

    add atributs for function fazendoChute in newmais.js

--- newmain.js
+++ newmain.js
@@ -1,19 +1,22 @@
 alert("Jogo de adivinhação, vamos jogar ? Adivinhe um numero de 1 a 7");
 
-let numeroPensado = Math.round((Math.random) * 7);
+let numeroPensado = Math.round(Math.random() * 7);
 let chute = parseInt(prompt("Chute um valor"));
 let tentativas = 3;
 
+function fazendoChute (){
 if (chute == numeroPensado){
     alert("Parabéns, você acertou!");
     document.write("<h1>Você acertou !!!<h1><br> O numero penssado foi " + numeroPensado);
-}
-else {
-    alert("Infelizmente você errou! Você terá mais uma chance.")
-    for (contador == 0; contador <= 3; contador ++){
-        chute;
+} else { while (tentativas >= 1) {
+    alert("Infelizmente você errou! Você terá mais " + tentativas + " chance.");
+    tentativas --;
+    let chute = parseInt(prompt("Chute um valor"));
+    if (chute == numeroPensado){
+        document.write("<h1>Você acertou !!!<h1><br> O numero penssado foi " + numeroPensado);
+        break
+            }
+        }
     }
 }
-
-
-
+fazendoChute();
