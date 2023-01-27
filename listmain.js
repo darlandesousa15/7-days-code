6c1b45a61b108ce8869ea40d0477c1c25a4b446c
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-26 22:12:49 -0300

    adicionando funcionalidades removendo bugs

--- listmain.js
+++ listmain.js
@@ -4,23 +4,20 @@ let categoriaProdutos = [];
 
 alert("Nesta página você irá criar sua lista de compras, onde iremos organizala e poupar sua caminhada e tempo.");
 
-function adicionandoLista(){
-    const adiconarlista = prompt("Deseja adiocar mais algum produto a sua lista ? Responda 'sim ou não'");
-    if (adiconarlista == "sim") {
+function adicionandoLista (){
+    const adicionarLista = prompt("Deseja adiocar mais algum produto a sua lista ? Responda 'sim ou não'");
+    while (adicionarLista == "sim" || adicionarLista == "s" ) {
         criandoLista();
-    } else {
-        document.write("<h1>Sua lista</h1><br>" + produtos " -- "+ categoriaProdutos);
+        if (adicionarLista == "não" || adicionarLista == "n" || adicionarLista == "nao" || adicionarLista == "ñ");
+    break;
     }
 }
 adicionandoLista();
+document.write(produtos + categoriaProdutos);
 
 function criandoLista () {
     produtos.push(prompt("Qual produto será adicionado?"));
     categoriaProdutos.push(prompt("Qual categoria este produto se encaixa ? 'fruta' 'legumes' 'laticinio' 'congelado' 'higiene' 'limpeza'"));
-    /*if (categoriaProdutos == "fruta" || adicionarCategoria == "legumes" || adicionarCategoria == "laticinio" || adicionarCategoria == "congelado" || adicionarCategoria == "higiene" || adicionarCategoria == "limpeza" || ) {
-
-    }*/
     adicionandoLista();
-    console.log(produtos, categoriaProdutos)
 }
 
