26a0a5ba5a1e73a341c67b3b845479bbfd3539ae
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-06 13:02:38 -0300

    Realizando alterações

--- mainlista.js
+++ mainlista.js
@@ -1,1 +1,19 @@
-alert(teste)
+const lista = [];
+const categoria = [];
+const fruta = [];
+const higiente = [];
+const limpeza = [];
+const congelado = [];
+
+
+alert("Crie uma lista automatica poupando traqbalho de organização")
+
+function testando (){
+	lista.prompt("Qual produto será adicionado a lista ?")
+	alert("Categorias disponiveis: - fruta - limpeza - higiene - congelado")
+	categoria.prompt("Qual categoria pertencia esse produto ?")
+	if (categoria === "fruta")
+		categoria.push(fruta)
+}
+
+testando();
