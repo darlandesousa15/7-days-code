693224e2eb2eb35f6501b254175516efa8b6c4d4
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-06 13:05:12 -0300

    alterando constantes

--- mainlista.js
+++ mainlista.js
@@ -1,5 +1,4 @@
 const lista = [];
-const categoria = [];
 const fruta = [];
 const higiente = [];
 const limpeza = [];
@@ -9,9 +8,9 @@ const congelado = [];
 alert("Crie uma lista automatica poupando traqbalho de organização")
 
 function testando (){
-	lista.prompt("Qual produto será adicionado a lista ?")
+	const lista = prompt("Qual produto será adicionado a lista ?")
 	alert("Categorias disponiveis: - fruta - limpeza - higiene - congelado")
-	categoria.prompt("Qual categoria pertencia esse produto ?")
+	const categoria = prompt("Qual categoria pertencia esse produto ?")
 	if (categoria === "fruta")
 		categoria.push(fruta)
 }
